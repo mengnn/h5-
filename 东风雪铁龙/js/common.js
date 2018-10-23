f93256147e6ed0099480fcc34ccea6a5f0f984c4
mengnn <fans_m@163.com>
2018-10-23 10:55:35 +0800

    h5特效 私人订制

---
+++ 东风雪铁龙/js/common.js
@@ -0,0 +1,12 @@
+/********************* 公共js*****************************/
+$(function(){
+    //返回首页
+$(".return").click(function(){
+    alert("1")
+    window.location.href="../menu.html";
+})
+//参会报名
+$(".mettingP").click(function(){
+    window.location.href="../mettingenlist.html"
+})
+})
